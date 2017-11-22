be26f45afdea7f28ad0c4c788688ef147929d049
Jason Tarre <jason.tarre@gmail.com>
2017-11-22 01:41:00 -0500

    Add database folder to house sqlite db plus init files (create_database.py and create_tables.py). Add sqlite init code to get-transcripts.py

--- balto/ted-scraping/get-transcripts.py
+++ balto/ted-scraping/get-transcripts.py
@@ -4,23 +4,47 @@ import re
 from bs4 import BeautifulSoup
 import sqlite3
 
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+ 
+    return None
 
-talkstxt="talks.txt"
-talks=open(talkstxt, 'r')
+def main():
+    database = "../../ted.db"
+ 
+    # create a database connection
+    conn = create_connection(database)
+    with conn:
+        talkstxt="talks.txt"
+        talks=open(talkstxt, 'r')
 
-for talk in talks:
-    talkclean = re.sub("\n","", talk)
-    transcript="https://www.ted.com" + talkclean + "/transcript?language=en"
-    print(transcript)
-    result = requests.get(transcript)
-    content = result.content
-    soup = BeautifulSoup(content, "html.parser")
-    talktxt = re.sub("/talks/","", talkclean)
-    talktxt = talktxt + ".txt"
-    print(talktxt)
-    talkfile = open("transcripts/" + talktxt, "w+")
-    talkfile.write(soup.title.string + "    " + talkclean)
-    talkfile.close()
-    print(soup.title.string)
-    time.sleep(1.5)
-talks.close()
+        for talk in talks:        
+            # create the ted talk url 
+            talkclean = re.sub("\n","", talk)
+            transcript="https://www.ted.com" + talkclean + "/transcript?language=en"
+            print(transcript)
+
+            # get the contents of the ted talk
+            # note: cannot get the transcript without 
+            # a library to interact with the TED React app
+            result = requests.get(transcript)
+            content = result.content
+
+            # parse the website content
+            soup = BeautifulSoup(content, "html.parser")
+            # todo: process the transcript
+            time.sleep(1.5)
+        talks.close()
+
+
+if __name__ == '__main__':
+    main()
